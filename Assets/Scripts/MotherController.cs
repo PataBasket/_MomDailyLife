6ae67b95867ae076eb150c221b175fc13feb74d2
PataBasket <basket2323yuta@gmail.com>
2023-12-20 03:02:58 +0900

    FPS camera and Map Camera implemented

--- Assets/Scripts/MotherController.cs
+++ Assets/Scripts/MotherController.cs
@@ -1,48 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 public class MotherController : MonoBehaviour
 {
-    [SerializeField] private float walkingSpeed = 1f;
+    float x, z;
+    float speed = 0.1f;
+
+    public GameObject cam;
+    Quaternion cameraRot, characterRot;
+    float Xsensityvity = 3f, Ysensityvity = 3f;
+
+    bool cursorLock = true;
+
+    //変数の宣言(角度の制限用)
+    float minX = -90f, maxX = 90f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraRot = cam.transform.localRotation;
+        characterRot = transform.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) 
-        {
-            Move(Vector3.forward);
-        }
+        float xRot = Input.GetAxis("Mouse X") * Ysensityvity;
+        float yRot = Input.GetAxis("Mouse Y") * Xsensityvity;
 
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-        {
-            Move(Vector3.back);
-        }
+        cameraRot *= Quaternion.Euler(-yRot, 0, 0);
+        characterRot *= Quaternion.Euler(0, xRot, 0);
 
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            Move(Vector3.right);
-        }
+        //Updateの中で作成した関数を呼ぶ
+        cameraRot = ClampRotation(cameraRot);
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            Move(Vector3.left);
-        }
+        cam.transform.localRotation = cameraRot;
+        transform.localRotation = characterRot;
+
+
+        UpdateCursorLock();
     }
 
-    void Move(Vector3 direction)
+    private void FixedUpdate()
     {
-        // プレイヤーの向きを指定の方向に向ける
-        transform.DOLookAt(transform.position + direction, 0.3f);
+        x = 0;
+        z = 0;
 
-        // プレイヤーを指定の方向に動かす
-        transform.Translate(direction * walkingSpeed * Time.deltaTime, Space.World);
+        x = Input.GetAxisRaw("Horizontal") * speed;
+        z = Input.GetAxisRaw("Vertical") * speed;
+
+        //transform.position += new Vector3(x,0,z);
+
+        transform.position += cam.transform.forward * z + cam.transform.right * x;
     }
+
+
+    public void UpdateCursorLock()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            cursorLock = false;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            cursorLock = true;
+        }
+
+
+        if (cursorLock)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else if (!cursorLock)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    //角度制限関数の作成
+    public Quaternion ClampRotation(Quaternion q)
+    {
+        //q = x,y,z,w (x,y,zはベクトル（量と向き）：wはスカラー（座標とは無関係の量）)
+
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1f;
+
+        float angleX = Mathf.Atan(q.x) * Mathf.Rad2Deg * 2f;
+
+        angleX = Mathf.Clamp(angleX, minX, maxX);
+
+        q.x = Mathf.Tan(angleX * Mathf.Deg2Rad * 0.5f);
+
+        return q;
+    }
+
+
 }
